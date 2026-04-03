6cdfb28b19aa24f8ef7eec2202c5dad18743b136
Luisa <luisa.couette@coda-student.school>
2026-04-03 10:42:22 +0200

    tableau dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -5,6 +5,7 @@ import {getProfileByUserId, type UserProfile} from "../services/profileService.t
 import {getThemeStats, type ThemeStat} from "../services/themeService.tsx";
 import {getTotalPoints, type TotalPoints} from "../services/questionnaire_sessionsService.tsx";
 import * as React from "react";
+import "../pages/dashboard.css"
 
 export function Dashboard() {
     const {session, signOut} = UserAuth();
@@ -78,6 +79,71 @@ export function Dashboard() {
             ) : (
                 <p>Aucune donnée du questionnaire pour le moment.</p>
             )}
+
+            <div className="table-container">
+                <ul className="custom-table">
+                    {/* En-tête du tableau */}
+                    <li className="custom-table-header">
+                        <div className="cellule">Thèmes</div>
+                        <div className="cellule">Mes points</div>
+                        <div className="cellule">Points de mon équipe</div>
+                    </li>
+                    <li className="Points-Rangée">
+                        <div className="cellule">Capacité d'innovation</div>
+                        <div className="cellule">50</div>
+                        <div className="cellule">20</div>
+                    </li>
+                    <li className="Points-Rangée">
+                        <div className="cellule">Gouvernance</div>
+                        <div className="cellule">30</div>
+                        <div className="cellule">18</div>
+                    </li>
+                    <li className="Points-Rangée">
+                        <div className="cellule">Performance</div>
+                        <div className="cellule">28</div>
+                        <div className="cellule">38</div>
+                    </li>
+                    <li className="Points-Rangée">
+                        <div className="cellule">Communication</div>
+                        <div className="cellule">8</div>
+                        <div className="cellule">11</div>
+                    </li>
+                    <li className="Points-Rangée">
+                        <div className="cellule">Appartenance</div>
+                        <div className="cellule">12</div>
+                        <div className="cellule">22</div>
+                    </li>
+                    <li className="Points-Rangée">
+                        <div className="cellule">Autonomie</div>
+                        <div className="cellule">9</div>
+                        <div className="cellule">12</div>
+                    </li>
+                    <li className="Points-Rangée">
+                        <div className="cellule">Développement des compétences</div>
+                        <div className="cellule">30</div>
+                        <div className="cellule">2</div>
+                    </li>
+                    <li className="Points-Rangée">
+                        <div className="cellule">Collaboration</div>
+                        <div className="cellule">19</div>
+                        <div className="cellule">20</div>
+                    </li>
+                    <li className="Points-Rangée">
+                        <div className="cellule">Adapation aux changements</div>
+                        <div className="cellule">17</div>
+                        <div className="cellule">19</div>
+                    </li>
+                    <li className="Points-Rangée">
+                        <div className="cellule">Motivation</div>
+                        <div className="cellule">40</div>
+                        <div className="cellule">25</div>
+                    </li>
+                </ul>
+            </div>
+
+
+
+
             <button onClick={() => setRole("Manager")}>Manager</button>
             <button onClick={() => setRole("Agent")}>Agent</button>
             <Link to="/questionnaire">Lancer le questionnaire</Link>
