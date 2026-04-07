6d3cb8fa2fc8c4e0e83f20bebaedebf701caf3bb
Luisa <luisa.couette@coda-student.school>
2026-04-07 16:07:50 +0200

    formation

--- src/pages/Formation.tsx
+++ src/pages/Formation.tsx
@@ -1,7 +1,9 @@
 export function Formation() {
     return (
         <div>
-            <h1>Page de formation</h1>
+            <h1>Catalogue de formations et accompagnements</h1>
+            <p>Découvrez l'ensemble des formations, ateliers et démarches d'accompagnement proposés par la Région Bourgogne-Franche-Comté pour développer vos pratiques managériales et renforcer la cohésion de vos équipes.</p>
+            <button>Thème 1</button>
         </div>
     )
 }
